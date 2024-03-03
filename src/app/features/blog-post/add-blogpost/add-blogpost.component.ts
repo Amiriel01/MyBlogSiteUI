745658c36f9d35439924edb5cc5f41c461563651
Amiriel01 <quinney.sarah@gmail.com>
2024-03-03 13:23:50 -0600

    created multi select menu to input categories

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../Services/blog-post.service';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit {
   model: AddBlogPost;
+  categories$?: Observable<Category[]>;
 
-  constructor(private blogPostService: BlogPostService, private router: Router) {
+  constructor(private blogPostService: BlogPostService, private router: Router, private cateogryService: CategoryService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -21,10 +25,15 @@ export class AddBlogpostComponent {
       author: '',
       isVisible: true,
       publishedDate: new Date(),
+      categories: [],
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.cateogryService.getAllCategories();
   }
 
   onFormSubmit(): void {
+    console.log(this.model)
     this.blogPostService.createBlogPost(this.model)
     .subscribe ({
       next: (response) => {
