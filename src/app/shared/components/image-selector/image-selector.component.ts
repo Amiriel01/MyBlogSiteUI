55f00510863f3d1b47aeb2a6d0f83a29cd102dbd
Amiriel01 <quinney.sarah@gmail.com>
2024-03-06 12:35:58 -0600

    hooked up form to submit image uploads

--- src/app/shared/components/image-selector/image-selector.component.ts
+++ src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { elementAt } from 'rxjs';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./image-selector.component.css']
 })
 export class ImageSelectorComponent {
+
+  private file?: File;
+  fileName: string = '';
+  title: string ='';
+
+  constructor(private imageService: ImageService) {
+
+  }
+
+  onFileUploadChange(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    this.file = element.files?.[0];
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName !== '' && this.title !== '') {
+      //image service to upload the image
+      this.imageService.uploadImage(this.file, this.fileName, this.title)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        }
+      });
+    }
+
+  }
 
 }
